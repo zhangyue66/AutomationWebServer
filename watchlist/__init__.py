97216b73f40b4d313c6411b55749af34ed3720b6
Yue Zhang <zhangyue19890116@gmail.com>
2021-05-02 18:30:55 -0700

    ready deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "dev"
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY', 'dev')
 
 #SQL lite URI compatible
 WIN = sys.platform.startswith("win")
@@ -16,7 +16,7 @@ else:
     prefix = "sqlite:////"
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
